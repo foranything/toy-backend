903ff4e091d26337ccbe00a99cded9a22565e55b
문민철 <bbomdam7@naver.com>
2020-07-16 22:32:13 +0900

    Webconfig firebase추가

--- src/main/java/com/kakao/config/WebConfig.java
+++ src/main/java/com/kakao/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		//냠냠냠냠
-		registry.addMapping("**").allowedOrigins("http://localhost:3000");
+		registry.addMapping("**").allowedOrigins("http://localhost:3000", "https://toy-chat-d6cbe.web.app/", "https://toy-chat-d6cbe.firebaseapp.com");
 	}
 }
