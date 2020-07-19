9a969d61a44a31c455277a84396009f1992e6644
WebFirewood <bsc1157@gmail.com>
2020-07-19 18:13:39 +0900

    test

--- src/main/java/com/kakao/KakaoCloneProjectApplication.java
+++ src/main/java/com/kakao/KakaoCloneProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KakaoCloneProjectApplication {
-	//test1
+	//test123
 	public static void main(String[] args) {
 		SpringApplication.run(KakaoCloneProjectApplication.class, args);
 	}
